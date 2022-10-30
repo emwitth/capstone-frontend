a795e028c5bf6dc21bc0bd2649a3336ee7a83e5b
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-10-30 16:30:43 -0500

    added package -- need to update

--- capstone/src/app/app.module.ts
+++ capstone/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { ResizableModule } from 'angular-resizable-element';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +26,7 @@ import { InfoPanelComponent } from './info-panel/info-panel.component';
     NgbModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ResizableModule,
     ToastrModule.forRoot({
       positionClass:'toast-top-right'
     })
