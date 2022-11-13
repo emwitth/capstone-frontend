5245c529e6b19d98c4a34f3fee90673449880236
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2022-11-13 16:24:56 -0600

    first implementation of look

--- capstone/src/app/hidden-items-list/hidden-items-list.component.ts
+++ capstone/src/app/hidden-items-list/hidden-items-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
+import { GraphJSON } from './../interfaces/d3-graph-interfaces';
 
 @Component({
   selector: 'app-hidden-items-list',
@@ -9,12 +10,19 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./hidden-items-list.component.css']
 })
 export class HiddenItemsListComponent implements OnInit {
+  hiddenItems: GraphJSON  = {
+    prog_nodes: [],
+    ip_nodes: [],
+    links: []
+  }
+
   constructor(public activeModal: NgbActiveModal, private http: HttpClient,
     private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    this.http.get<any>("api/hidden_items" , { observe: "response" }).subscribe(result => {
+    this.http.get<GraphJSON>("api/hidden_items" , { observe: "response" }).subscribe(result => {
       console.log(result.body);
+      this.hiddenItems = result.body ? result.body : this.hiddenItems;
       }, err => {
         this.toastr.error(err.status + " " + err.statusText, "Error!");
         console.log(err);
--- capstone/src/app/interfaces/link.ts
+++ capstone/src/app/interfaces/link.ts
@@ -2,6 +2,7 @@ import { ProgInfo } from "./prog-node";
 
 export interface Link {
     ip: string,
+    ip_name: string,
     program: ProgInfo,
     in_packets: number,
     out_packets: number,
