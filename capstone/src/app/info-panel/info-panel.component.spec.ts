1cabdd55a69ea2f7fed72ba66b85cf8ffed2b2df
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2023-02-26 13:03:28 -0600

    All initial tests pass.

--- capstone/src/app/info-panel/info-panel.component.spec.ts
+++ capstone/src/app/info-panel/info-panel.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 import { InfoPanelComponent } from './info-panel.component';
 
 describe('InfoPanelComponent', () => {
   let component: InfoPanelComponent;
   let fixture: ComponentFixture<InfoPanelComponent>;
+  let httpMock: HttpTestingController;
+  let toastrService: ToastrService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InfoPanelComponent ]
+      declarations: [ InfoPanelComponent ],
+      imports: [HttpClientTestingModule, ToastrModule],
+      providers: [{provide: HttpClient, useValue: httpMock},
+                  {provide: ToastrService, useValue: toastrService}]
     })
     .compileComponents();
   });
@@ -17,6 +25,8 @@ describe('InfoPanelComponent', () => {
     fixture = TestBed.createComponent(InfoPanelComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpMock = TestBed.inject(HttpTestingController);
+    toastrService = TestBed.inject(ToastrService);
   });
 
   it('should create', () => {
--- capstone/src/app/toolbar/toolbar.component.spec.ts
+++ capstone/src/app/toolbar/toolbar.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { ToastrService, ToastrModule } from 'ngx-toastr';
 
 import { ToolbarComponent } from './toolbar.component';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
   let fixture: ComponentFixture<ToolbarComponent>;
+  let httpMock: HttpTestingController;
+  let toastrService: ToastrService;
+  let httpClient: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ToolbarComponent ]
+      declarations: [ ToolbarComponent ],
+      imports: [HttpClientTestingModule, ToastrModule],
+      providers: [{provide: HttpClient, useValue: httpClient},
+                  {provide: ToastrService, useValue: toastrService}]
     })
     .compileComponents();
   });
@@ -17,6 +26,9 @@ describe('ToolbarComponent', () => {
     fixture = TestBed.createComponent(ToolbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient =TestBed.inject(HttpClient);
+    toastrService = TestBed.inject(ToastrService);
   });
 
   it('should create', () => {
