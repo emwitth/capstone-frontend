eaffa53866c112af9c52e8df733cf9586c6a2849
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2023-03-23 21:12:18 -0500

    butified styles

--- capstone/src/app/save/save.component.ts
+++ capstone/src/app/save/save.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-save',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./save.component.css']
 })
 export class SaveComponent implements OnInit {
+  wantSave:boolean = false;
 
-  constructor() { }
+  constructor(public activeModal: NgbActiveModal) { }
 
   ngOnInit(): void {
+  }
+
+  save() {
+    
   }
 
 }
