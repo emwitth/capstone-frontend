b776909b730feffa84bdbc39a1989cd0192e9654
Evan Witthun <98661203+emwitth@users.noreply.github.com>
2023-03-30 16:29:56 -0500

    added delete sessions functionality

--- capstone/src/app/session-list/session-list.component.ts
+++ capstone/src/app/session-list/session-list.component.ts
@@ -14,13 +14,30 @@ export class SessionListComponent implements OnInit {
   constructor(private toastr: ToastrService, private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.loadSessions();
+  }
+
+  loadSessions() {
     this.http.get<Array<Session>>("api/sessions" , { observe: "response" }).subscribe(result => {
       console.log(result.body);
       this.sessions = result.body ? result.body : [];
     }, err => {
-        this.toastr.error(err.status + " " + err.statusText, "Error!");
-        console.log(err);
-      });
+      this.toastr.error(err.status + " " + err.statusText, "Error!");
+      console.log(err);
+    });
+  }
+
+  delete(name:string) {
+    console.log(name)
+    this.http.delete<any>("api/sessions/" + name , { observe: "response" }).subscribe(result => {
+      console.log(result.body);
+      this.toastr.success(result.body);
+      this.loadSessions();
+    }, err => {
+      this.toastr.error(err.status + " " + err.statusText, "Error!");
+      console.log(err);
+    });
+    this.loadSessions();
   }
 
 }
